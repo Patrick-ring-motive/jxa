ccbeebea7a7f03f0b2063adcf61afe93b56815f9
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-09-03 21:41:28 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+ObjC.import('stdio');
+
+function run() {
+	// Hardcoded placeholder for what will eventually be a submitted DSL script.
+	const script = "print 1 + 1";
+
+	$.puts("dslementary: executing hardcoded script");
+	$.puts("script: " + script);
+	$.puts("result: 2");
+}
+
+run();
